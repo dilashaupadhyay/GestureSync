fd0d1ee00b6904d5a14e29362863417a6aaeb18a
@SaPok5 <samippokhrel5@gmail.com>
2023-09-29 21:03:10 +0545

    Samip

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import os
+from django.conf import settings
+
 
 # Create your views here.
 def index(request):
     return render(request, 'build/index.html')
+
+
+def CreateKeyword(request):
+    DATA_PATH = os.path.join('MP_DATA')
+
+    no_of_sequences = 30
+    sequence_length = 30
+    start_folder = 30
+
+    if request.method == "POST":
+        keyword = request.POST.get('keyword')
+        
+        for sequence in range(1, no_of_sequences+1):
+            try:
+                os.makedirs(os.path.join(DATA_PATH, keyword, str(sequence)))
+            except:
+                pass
