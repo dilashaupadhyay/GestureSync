0a2ac41a00dd731bb9eb0ea97ba6fa27d5d22cd9
Shishir Tamrakar <stamrakar545@gmail.com>
2023-09-30 14:07:26 +0545

    add record samples

--- backend/main/consumers.py
+++ backend/main/consumers.py
@@ -1,6 +1,25 @@
 import json
+import base64
+import os
+import numpy as np
 
 from channels.generic.websocket import WebsocketConsumer
+
+dataDir = os.path.join(os.getcwd(), 'datasets')
+
+def fileCounter(path):
+    count = 0;
+    for file in (os.listdir(path)):
+        if os.path.isfile(os.path.join(path, file)):
+            count += 1;
+    return count
+
+def folderCounter(path):
+    count = 0
+    for file in (os.listdir(path)):
+        if not os.path.isfile(os.path.join(path, file)):
+            count += 1
+    return count
 
 class VideoStreamConsumer(WebsocketConsumer):
     def connect(self):
@@ -11,9 +30,43 @@ class VideoStreamConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         data = json.loads(text_data)
+        sampleNo = data.get('sampleNo', None)
+        frameNo = data.get('frameCount', None)
+        keyword = data.get('keyword', None)
+        event = data.get('event', None)
         jpeg_base64 = data.get('jpeg_base64', None)
-        counter = data.get('counter')
 
         if jpeg_base64:
-            self.send(text_data=json.dumps({"message": "Frame received"}))
-            print(counter)
+            if event == 'record':
+                try:
+                    image_data = base64.b64decode(jpeg_base64)
+                except Exception as e:
+                    response_data = {'error': 'Invalid base64 encoding!'}
+                    self.send(text_data=json.dumps(response_data))
+                    return
+
+                try:
+                    image_np = np.frombuffer(image_data, np.uint8)
+                except Exception as e:
+                    response_data = {'error': 'Failed to convert image to NumPy array!'}
+                    self.send(text_data=json.dumps(response_data))
+                    return
+
+                directory = os.path.join(dataDir, keyword, str(sampleNo))
+                os.makedirs(directory, exist_ok=True)
+
+                filename = str(frameNo) + '.npy'
+
+                npy_path = os.path.join(directory, filename)
+
+                try:
+                    np.save(npy_path, image_np)
+                except Exception as e:
+                    response_data = {'error': 'Failed to save the .npy file'}
+                    self.send(text_data=json.dumps(response_data))
+                    return
+                
+                response_data = {'message': 'NumPy .npy file saved successfully'}
+                self.send(text_data=json.dumps(response_data))
+
+        self.send(text_data=json.dumps({"message": "Frame received"}))
--- frontend/src/WebCam.js
+++ frontend/src/WebCam.js
@@ -1,7 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 
 const WebcamComponent = () => {
+    const [count, setCount] = useState(0);
+    const [canChangeKeyword, setCanChangeKeyword] = useState(true);
+
+    const [keyword, setKeyword] = useState('');
+    const [sampleNo, setSampleNo] = useState(0);
     const webcamRef = useRef(null);
 
     const webcamSocket = new WebSocket(
@@ -10,11 +15,27 @@ const WebcamComponent = () => {
         + '/ws/main/'
     )
 
-    const startStream = async () => {
+    const handleKeywordChange = (e) => {
+        setKeyword(e.target.value);
+    };
+
+    const handleButtonClick = () => {
+        if (count < 10) {
+            setCanChangeKeyword(false);
+            setCount(count + 1);
+
+            if (count === 9) {
+                setCount(0);
+                setCanChangeKeyword(true);
+            }
+        }
+  };
+
+    const startStream = async (event) => {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true})
         webcamRef.current.video.srcObject = stream;
 
-        const captureFrames = () => {
+        const captureFrames = (frameCount=0) => {
             const canvas = document.createElement('canvas');
             const video = webcamRef.current.video;
             
@@ -26,14 +47,20 @@ const WebcamComponent = () => {
             const imageData = canvas.toDataURL('image/jpeg');
 
             const data = {
+                sampleNo: sampleNo,
+                frameCount: frameCount,
+                keyword: keyword,
+                event: event,
                 jpeg_base64: imageData,
             }
             webcamSocket.send(JSON.stringify(data))
-            console.log('frame sent')
+            console.log(event)
         };
 
-        for(let i = 0; i < 1000; i++) {
-            captureFrames()
+        if(event == 'record') {
+            for(let i = 0; i < 10; i++) {
+                captureFrames(i)
+            }
         }
     };
 
@@ -44,7 +71,18 @@ const WebcamComponent = () => {
                 ref={webcamRef}
                 style={{width:640, height:480}}
             />
-            <button onClick={startStream}>Translate</button>
+            <br/>
+            <button onClick={() => startStream('translate')}>Translate</button>
+            <input type='text' onChange={handleKeywordChange} value={keyword} disabled={!canChangeKeyword}></input>
+            <button 
+                onClick={() => {
+                    startStream('record')
+                    handleButtonClick()
+                    if(sampleNo != 10)
+                        setSampleNo(sampleNo + 1)
+                    else
+                        setSampleNo(0)
+            }}>Record</button>
         </div>
     );
 }
